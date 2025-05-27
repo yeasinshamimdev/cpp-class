03ac95325066c72e6674e17b890f7d4e16858c73
yeasinshamimdev <yeasinshamim7@gmail.com>
2025-05-27 18:00:35 +0600

    extra practice problem done

---
+++ module-2.5/problem_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int* get_array (int n){
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    return arr;
+    
+}
+
+int main (){
+    int n;
+    cin >> n;
+    int* a = get_array(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    
+
+    return 0;
+}
